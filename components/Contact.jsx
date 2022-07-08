f973d07f2a6da602a35248b6543a3455c634b32f
matthieubrenne <matthieubrenne2103@gmail.com>
2022-07-08 17:38:38 +0200

    contact form sendGrid config

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,11 +1,33 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 import contactImg from '../public/assets/contact.png';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const data = {
+      name,
+      phone,
+      email,
+      subject,
+      message,
+    };
+    
+    fetch('/api/contact', {
+      method: 'post',
+      body: JSON.stringify(data),
+    });
+  };
+
   return (
     <div id="contact" className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
@@ -26,7 +48,7 @@ const Contact = () => {
                   height="400"
                 />
               </div>
-              <div>
+              <div>aaaaaa
                 <h2 className="py-2">Matthieu Brenne</h2>
                 <p>Développeur Front-End</p>
                 <p className="py-4">
@@ -56,57 +78,95 @@ const Contact = () => {
           {/* right */}
           <div className="col-span-3 w-full h-auto shadow-gray-400 rounded-xl lg:p-4 pt-40">
             <div className="p-4">
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="grid md-grid-cols-2 gap-4 w-full py-2">
                   <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">
+                    <label
+                      className="uppercase text-sm py-2"
+                      htmlFor="name"
+                    >
                       Nom
                     </label>
                     <input
+                      id="name"
                       className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
+                      onChange={(e) => {
+                        setName(e.target.value);
+                      }}
                     />
                   </div>
                 </div>
                 <div className="grid md-grid-cols-2 gap-4 w-full py-2">
                   <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">
+                    <label
+                      className="uppercase text-sm py-2"
+                      htmlFor="phone"
+                    >
                       Telephone
                     </label>
                     <input
+                      id="phone"
                       className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
+                      onChange={(e) => {
+                        setPhone(e.target.value);
+                      }}
                     />
                   </div>
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">
+                  <label
+                    className="uppercase text-sm py-2"
+                    htmlFor="email"
+                  >
                     Email
                   </label>
                   <input
+                    id="email"
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="email"
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">
+                  <label
+                    className="uppercase text-sm py-2"
+                    htmlFor="subject"
+                  >
                     Sujet
                   </label>
                   <input
+                    id="subject"
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="text"
+                    onChange={(e) => {
+                      setSubject(e.target.value);
+                    }}
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">
+                  <label
+                    className="uppercase text-sm py-2"
+                    htmlFor="message"
+                  >
                     Message
                   </label>
                   <textarea
+                    id="message"
                     className="border-2 rounded-lg p-3 border-gray-300"
                     rows="10"
+                    onChange={(e) => {
+                      setMessage(e.target.value);
+                    }}
                   ></textarea>
                 </div>
-                <button className="w-full p-4 text-gray-100 mt-4">
+                <button
+                  className="w-full p-4 text-gray-100 mt-4"
+                  // type="submit"
+                >
                   Envoyer le message
                 </button>
               </form>
@@ -115,7 +175,7 @@ const Contact = () => {
         </div>
         <div className="flex justify-center py-12">
           <Link href="/">
-            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+            <div className="animate-bounce rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
               <HiOutlineChevronDoubleUp
                 className="text-[#5651e5]"
                 size={30}
