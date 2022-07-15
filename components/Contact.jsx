690602eab83dfa7d46fd300be199bbe075d09456
matthieubrenne <matthieubrenne2103@gmail.com>
2022-07-15 17:19:09 +0200

    contact form sendGrid config and fix

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 import contactImg from '../public/assets/contact.png';
